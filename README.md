ff287b11a887cfc39b481c518c691e82aed745ff
Maddy Ford <mrhf008@gmail.com>
2018-05-13 10:10:50 -0700

    completed landing page with glyphicons, decided tomake two stylesheets, renamed appropriately

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # supergirl
+
+colors:
+slate: #333333
+bright pink: #F76F9A
+pale pink: #FFDAD8
+white: #FFFFFF
+blue: #55DDEO/#55DDE0
--- app/routing/routing.js
+++ app/routing/routing.js
@@ -1,1 +1,22 @@
 // default page = index/landing
+var path = require('path');
+
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../public/index.html'));
+  });
+
+  app.get('/contact', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../public/contact.html'));
+  });
+
+  app.get('/portfolio', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../public/portfolio.html'));
+  });
+  app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../public/about.html'));
+  });
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '/../public/index.html'));
+  });
+};
