70e529e8e9295eed154c631daa095084311849ae
Maddy Ford <mrhf008@gmail.com>
2018-05-17 12:58:43 -0700

    added concatenation lines for blog, contact, portfolio and successfully tested

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+cat templates/top.html content/portfolio.html templates/bottom.html > docs/portfolio.html
