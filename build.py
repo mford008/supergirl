ed32d93e8bad206a9d88de178f2a241c989510e7
Maddy Ford <mrhf008@gmail.com>
2018-05-20 13:56:32 -0700

    added python code for ssg

--- build.py
+++ build.py
@@ -1,7 +1,24 @@
-cat templates/top.html content/about.html templates/bottom.html > docs/about.html
-cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
-cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
-cat templates/top.html content/portfolio.html templates/bottom.html > docs/portfolio.html
-cat templates/top-landing.html content/index.html > docs/index.html
+top_template_landing = open('templates/top-landing.html').read()
+bottom_template = open('templates/bottom.html').read()
+content = open('content/index.html').read()
 
-# Have python code here!
+index_html = top_template_landing + content + bottom_template
+open('docs/index.html', 'w+').write(index_html)
+
+top_template = open('templates/top.html').read()
+# bottom_template = open('templates/bottom.html').read()
+content = open('content/about.html').read()
+about_html = top_template + content + bottom_template
+open('docs/about.html', 'w+').write(about_html)
+
+content = open('content/blog.html').read()
+blog_html = top_template + content + bottom_template
+open('docs/about.html', 'w+').write(about_html)
+
+content = open('content/contact.html').read()
+contact_html = top_template + content + bottom_template
+open('docs/about.html', 'w+').write(contact_html)
+
+content = open('content/portfolio.html').read()
+portfolio_html = top_template + content + bottom_template
+open('docs/portfolio.html', 'w+').write(portfolio_html)
