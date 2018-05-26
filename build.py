8ba66577e22c44183b5ae12697af2f994d904f5a
Maddy Ford <mrhf008@gmail.com>
2018-05-26 11:30:27 -0700

    minor syntax error corrections

--- build.py
+++ build.py
@@ -1,28 +1,28 @@
 def main():
-top_template_landing = open('templates/top-landing.html').read()
-bottom_template = open('templates/bottom.html').read()
-content = open('content/index.html').read()
+    top_template_landing = open('templates/top-landing.html').read()
+    bottom_template = open('templates/bottom.html').read()
+    content = open('content/index.html').read()
 
-index_html = top_template_landing + content + bottom_template
-open('docs/index.html', 'w+').write(index_html)
+    index_html = top_template_landing + content + bottom_template
+    open('docs/index.html', 'w+').write(index_html)
 
-top_template = open('templates/top.html').read()
-# bottom_template = open('templates/bottom.html').read()
-content = open('content/about.html').read()
-about_html = top_template + content + bottom_template
-open('docs/about.html', 'w+').write(about_html)
+    top_template = open('templates/top.html').read()
+    # bottom_template = open('templates/bottom.html').read()
+    content = open('content/about.html').read()
+    about_html = top_template + content + bottom_template
+    open('docs/about.html', 'w+').write(about_html)
 
-content = open('content/blog.html').read()
-blog_html = top_template + content + bottom_template
-open('docs/blog.html', 'w+').write(blog_html)
+    content = open('content/blog.html').read()
+    blog_html = top_template + content + bottom_template
+    open('docs/blog.html', 'w+').write(blog_html)
 
-content = open('content/contact.html').read()
-contact_html = top_template + content + bottom_template
-open('docs/contact.html', 'w+').write(contact_html)
+    content = open('content/contact.html').read()
+    contact_html = top_template + content + bottom_template
+    open('docs/contact.html', 'w+').write(contact_html)
 
-content = open('content/portfolio.html').read()
-portfolio_html = top_template + content + bottom_template
-open('docs/portfolio.html', 'w+').write(portfolio_html)
+    content = open('content/portfolio.html').read()
+    portfolio_html = top_template + content + bottom_template
+    open('docs/portfolio.html', 'w+').write(portfolio_html)
 
-if __name__ = __'main'__:
+if __name__ == '__main__':
     main()
