372c05c8693bff8cb8c5c00ae7ee753d83d57c29
Maddy Ford <mrhf008@gmail.com>
2018-05-27 14:34:03 -0700

    edits to build.py for functionality with assembling all docs pages

--- build.py
+++ build.py
@@ -2,18 +2,22 @@ def main():
 
     pages = [
         {
+        'title': 'About',
         'filename': 'content/about.html',
         'output_filename': 'docs/about.html',
         },
         {
+        'title': 'Blog',
         'filename': 'content/blog.html',
         'output_filename': 'docs/blog.html',
         },
         {
+        'title': 'Contact',
         'filename': 'content/contact.html',
         'output_filename': 'docs/contact.html',
         },
         {
+        'title': 'Portfolio',
         'filename': 'content/portfolio.html',
         'output_filename': 'docs/portfolio.html',
         }
@@ -24,7 +28,11 @@ def main():
         content = open(x['filename']).read()
         finished_page = template.replace('{{content}}', content)
         open(x['output_filename'], 'w+').write(finished_page)
-        
+
+        template = open('templates/base-landing.html').read()
+        content = open('content/index.html').read()
+        finished_landing_page = template.replace('{{content}}', content)
+        open('docs/index.html', 'w+').write(finished_landing_page)
     # top_template_landing = open('templates/top-landing.html').read()
     # bottom_template_landing = open('templates/bottom-landing.html').read()
     # content = open('content/index.html').read()
