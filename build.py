ecc182d159ffb42441eb162698e80f3481d5e37b
Maddy Ford <mrhf008@gmail.com>
2018-05-27 13:55:47 -0700

    combined files, successful generation of new doc files

--- build.py
+++ build.py
@@ -19,20 +19,25 @@ def main():
         }
     ]
 
-
-    top_template_landing = open('templates/top-landing.html').read()
-    bottom_template_landing = open('templates/bottom-landing.html').read()
-    content = open('content/index.html').read()
-    index_html = top_template_landing + content + bottom_template_landing
-    open('docs/index.html', 'w+').write(index_html)
-
     for x in pages:
-        top_template = open('templates/top.html').read() #still need to establish top and bottom templates
-        bottom_template = open('templates/bottom.html').read()
+        template = open('templates/base.html').read()
         content = open(x['filename']).read()
-        newpage = top_template + content + bottom_template
-        open(x['output_filename'], 'w+').write(newpage)
-        print('success!')
+        finished_page = template.replace('{{content}}', content)
+        open(x['output_filename'], 'w+').write(finished_page)
+        
+    # top_template_landing = open('templates/top-landing.html').read()
+    # bottom_template_landing = open('templates/bottom-landing.html').read()
+    # content = open('content/index.html').read()
+    # index_html = top_template_landing + content + bottom_template_landing
+    # open('docs/index.html', 'w+').write(index_html)
+    #
+    # for x in pages:
+    #     top_template = open('templates/top.html').read() #still need to establish top and bottom templates
+    #     bottom_template = open('templates/bottom.html').read()
+    #     content = open(x['filename']).read()
+    #     newpage = top_template + content + bottom_template
+    #     open(x['output_filename'], 'w+').write(newpage)
+    #     print('success!')
 
 if __name__ == '__main__':
     main()
