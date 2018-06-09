f2c8f12262a70520b604b1a53f60ac078c060cea
Maddy Ford <mrhf008@gmail.com>
2018-06-09 14:47:00 -0700

    more gross changes

--- build.py
+++ build.py
@@ -3,27 +3,34 @@ import os
 from jinja2 import Template
 import markdown
 
-site = {
-    'base': 'templates/base.html',
-    'pages': [],
-}
+pages = []
 landing = {
     'base': 'templates/base-landing.html',
     'pages': [],
 }
 def main():
-    all_content_files = glob.glob('content/*.html')
+    #grabbing all files, saving as variable
+    all_content_files = glob.glob('content/*.md')
+    #doing same for landing file
     landing_file = glob.glob('landing/*.html')
-    # establishing list
+    # establishing list by looping through list variable
     for page in all_content_files:
         file_name = os.path.basename(page)
+        # print('filename', file_name) #filename.ext
         name_only, extension = os.path.splitext(file_name)
-        collect(page, file_name, name_only)
+        # print('name only', name_only) #filename
+        # print('extension', extension)#.md
+        #appending information to site['pages'] for each page
+        collect(page, file_name, name_only) #makes a list of dictionaries
     #using list
-    for page in site['pages']:
-        template(site['pages']['filename']) #what needs to go in place of filename?
+    for page in pages:
+        # print('meow', filename)
+        # print('meow', [filename])
+        # print('meow', ['filename'])
+        print('TEST', page['filename'])
+        template(page['filename']) #what needs to go in place of filename?
         # convert(???)
-        open(site['pages']['filepath'], 'w+').write(site['pages']['templated_content'])
+        open(page['filepath'], 'w+').write(page['templated_content'])
     for page in landing_file:
         file_name = os.path.basename(page)
         name_only, extension = os.path.splitext(file_name)
@@ -33,12 +40,13 @@ def main():
         open(landing['pages']['filepath'], 'w+').write(landing['pages']['templated_content'])
 
 def collect(page, file_name, name_only):
-    site['pages'].append({
+    pages.append({
         'filename': str(page),
         'filepath': 'docs/'+str(file_name),
         'title': 'Maddy Ford - ' + name_only,
     })
-    print(['filename'])
+    # print('page', page) #filename.ext
+    # print('test', site['pages']) #prints a list of dictionaries
 def collect_landing(page, file_name, name_only):
     landing['pages'] = {
         'filename': str(page),
@@ -47,16 +55,17 @@ def collect_landing(page, file_name, name_only):
     }
 
 def template(page):
-    content_html = open(site['pages']['filename']).read() #what needs to go in place of filename?
-    template_html = open(site['base']).read()
+    # print(type(['filename']))
+    content_html = open(page['filename']).read() #what needs to go in place of filename?
+    template_html = open('templates/base.md').read()
     template = Template(template_html)
     finished_page = template.render(
-        title = site['pages']['title'],
+        title = pages['title'],
         content = content_html,
-        filepath = site['pages']['filepath'],
-        pages = site['pages'],
+        filepath = pages['filepath'],
+        pages = pages,
     )
-    site['pages'].update({'templated_content': finished_page})
+    pages.update({'templated_content': finished_page})
 def template_landing(page):
     content_html = open(page).read()
     template_html = open(landing['base']).read()
