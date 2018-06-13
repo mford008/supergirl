04100c6f5ffbe912b2a9964430e2ffd0e6fed8d2
Maddy Ford <mrhf008@gmail.com>
2018-06-12 17:06:14 -0700

    able to generate blog posts with function in build

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ import os
 from jinja2 import Template
 import markdown
 
+blog_posts = []
 pages = []
 landing = {
     'base': 'templates/base-landing.html',
@@ -39,6 +40,16 @@ def append(file_name, name_only):
         'title': 'Maddy Ford - ' + name_only,
     })
 
+def blog_append(file_name, name_only):
+    blog_posts.append({
+        'filepath': 'blog_posts/'+str(file_name),
+        'filename': file_name,
+        'name': name_only,
+        'output_filepath': 'docs/'+str(name_only) + '.html',
+        'output_filename': str(name_only) + '.html',
+        'title': 'Maddy Ford - Blog',
+    })
+
 def convert(page):
     md = markdown.Markdown(extensions=["markdown.extensions.meta"])
     md_content = open(page).read()
@@ -65,6 +76,28 @@ def landing():
     open('docs/index.html', 'w+').write(finished_landing_page)
 
 landing()
+
+def compile_blog_posts():
+    all_blog_posts = glob.glob('blog_posts/*.md')
+    for page in all_blog_posts:
+        file_name = os.path.basename(page)
+        # print('filename', file_name)
+        name_only, extension = os.path.splitext(file_name)
+        # print('name', name_only)
+        convert(page)
+        blog_append(file_name, name_only)
+    for page in blog_posts:
+        content_html = open(page['filepath']).read()
+        template_html = open('templates/base.html').read()
+        template = Template(template_html)
+        finished_blog_page = template.render(
+            title = page['title'],
+            content = content_html,
+            filepath = page['output_filepath'],
+        )
+        open(page['output_filepath'], 'w+').write(finished_blog_page)
+
+compile_blog_posts()
 
 if __name__ == '__main__':
     main()
